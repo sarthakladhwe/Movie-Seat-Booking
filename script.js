fc217c71d3ac5cec381df2b1526d5228f100afbd
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2021-10-05 16:41:29 +0530

    Implemented local storage
    The local storage will save selected seats value and total movie cost

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 //Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
@@ -18,7 +29,7 @@ function updateSelectedCount() {
 //Movie select event
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = +e.target.value;
-
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
